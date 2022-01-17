75cafd4168367bb364cc0413707e27567c1628e6
AmitFikler <amitfikler@gmail.com>
2022-01-17 14:35:57 +0200

    ⬇️ Install and use cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,16 @@
 import express from 'express';
-require('dotenv').config();
+import cors from 'cors';
+import errorHandler from './utils/middleware/errorHandlerMiddleware';
+
+import config from './utils/config';
 
 const app = express();
 app.use(express.json()); //json middleware
+app.use(cors()); //cors middleware
 
-app.listen(process.env.PORT, () => {
-  console.log(`running on http://localhost:${process.env.PORT}/`);
+app.use(errorHandler); //error handler middleware
+
+// start server
+app.listen(config.port, () => {
+  console.log(`running on http://localhost:${config.port}/`);
 });
