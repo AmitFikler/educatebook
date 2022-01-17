938a33877fce71f0d0c7857c3f646b8540c5b359
AmitFikler <amitfikler@gmail.com>
2022-01-17 15:09:32 +0200

    🗄️ Connect to mongodb

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,29 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 import errorHandler from './utils/middleware/errorHandlerMiddleware';
 import config from './utils/config';
 
 import apiRouter from './routers/api';
 
+const MONGO_URI = config.mongodb_url;
+const PORT = config.port;
+
 const app = express();
+if (MONGO_URI && PORT) {
+  mongoose
+    .connect(MONGO_URI) // connect to mongodb
+    .then(() => {
+      console.log(`connected to MongoDB - ${config.mongodb_url}`);
+      app.listen(config.port, () =>
+        console.log(`app listening at http://localhost:${config.port}`)
+      );
+    })
+    .catch((error) => {
+      console.log('error connecting to MongoDB:', error.message);
+    });
+}
 
 app.use(cors()); //cors middleware
 app.use(express.json()); //json middleware
@@ -16,6 +33,3 @@ app.use('/api', apiRouter);
 app.use(errorHandler); //error handler middleware
 
 // start server
-app.listen(config.port, () => {
-  console.log(`running on http://localhost:${config.port}/`);
-});
--- backend/src/utils/config/index.ts
+++ backend/src/utils/config/index.ts
@@ -3,4 +3,5 @@ dotenv.config();
 
 export default {
   port: process.env.PORT,
+  mongodb_url: process.env.MONGODB_URL,
 };
