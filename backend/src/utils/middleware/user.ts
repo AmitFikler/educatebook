238395a55bb86bff4a1abaa90cca5d3683483270
AmitFikler <amitfikler@gmail.com>
2022-01-24 17:57:43 +0200

    🧪 Testing post a post

--- backend/src/utils/middleware/user.ts
+++ backend/src/utils/middleware/user.ts
@@ -4,7 +4,11 @@ import { tokenExtractor } from '../helpers/jwtService';
 const userFinder = async (req: Request, _res: Response, next: NextFunction) => {
   const authorization = req.get('authorization');
   if (authorization == null) throw { status: 401, message: 'Token missing' };
-  req.decodedToken = tokenExtractor(authorization);
+  try {
+    req.decodedToken = tokenExtractor(authorization);
+  } catch (error) {
+    next(error);
+  }
   next();
 };
 
--- backend/tests/user.test.ts
+++ backend/tests/user.test.ts
@@ -1,9 +1,11 @@
 import mongoose from 'mongoose';
 import supertest from 'supertest';
 import { app } from '../src/index';
+import { Post } from '../src/models/Post';
 import { User } from '../src/models/User';
 const api = supertest(app);
 
+const userToken: { token: String } = { token: '' };
 const users = [
   {
     _id: '61e58ae42184d73091dcf6bb',
@@ -20,44 +22,15 @@ const users = [
 
 beforeAll(async () => {
   await User.deleteMany({});
+  await Post.deleteMany({});
   await User.insertMany(users);
 });
 
-describe('user testing:', () => {
+describe('USER TESTING:', () => {
   describe('GET /api/user:', () => {
     it('should be 1 user', async () => {
       const response = await api.get('/api/user');
       expect(response.body).toHaveLength(1);
-    });
-  });
-  describe('POST /api/login:', () => {
-    it('should login with current details and get token', async () => {
-      const response = await api.post('/api/login').send({
-        username: 'amit@gmail.com',
-        password: '123456',
-      });
-      let hasToken = response.body.hasOwnProperty('token');
-      expect(hasToken).toBe(true);
-    });
-    it('should throw (403) with incurrent details (password)', async () => {
-      const response = await api
-        .post('/api/login')
-        .send({
-          username: 'amit@gmail.com',
-          password: 'NOT_PASSWORD',
-        })
-        .expect(403);
-      expect(response.body.error).toBe('wrong password');
-    });
-    it('should throw (403) with incurrent details (username)', async () => {
-      const response = await api
-        .post('/api/login')
-        .send({
-          username: 'amitFAIL@gmail.com',
-          password: '123456',
-        })
-        .expect(403);
-      expect(response.body.error).toBe('wrong username');
     });
   });
   describe('POST /api/user:', () => {
@@ -81,6 +54,86 @@ describe('user testing:', () => {
           password: 'test123',
           role: 'student',
         })
+        .expect(400);
+    });
+  });
+});
+describe('LOGIN TESTING', () => {
+  describe('POST /api/login:', () => {
+    it('should login with current details and get token', async () => {
+      const response = await api.post('/api/login').send({
+        username: 'amit@gmail.com',
+        password: '123456',
+      });
+      let hasToken = response.body.hasOwnProperty('token');
+      userToken.token = response.body.token;
+      expect(hasToken).toBe(true);
+    });
+    it('should throw (403) with incurrent details (password)', async () => {
+      const response = await api
+        .post('/api/login')
+        .send({
+          username: 'amit@gmail.com',
+          password: 'NOT_PASSWORD',
+        })
+        .expect(403);
+      expect(response.body.error).toBe('wrong password');
+    });
+    it('should throw (403) with incurrent details (username)', async () => {
+      const response = await api
+        .post('/api/login')
+        .send({
+          username: 'amitFAIL@gmail.com',
+          password: '123456',
+        })
+        .expect(403);
+      expect(response.body.error).toBe('wrong username');
+    });
+  });
+});
+
+describe('POST TESTING:', () => {
+  describe('GET /api/post', () => {
+    it('should be 0 posts', async () => {
+      const allPosts = await api.get('/api/post');
+      expect(allPosts.body).toHaveLength(0);
+    });
+  });
+
+  describe('POST /api/post:', () => {
+    it('should user with valid token can post a valid post', async () => {
+      const response = await api
+        .post('/api/post')
+        .send({
+          title: 'valid post',
+          content: 'testing',
+        })
+        .set({ Authorization: 'Bearer ' + userToken.token })
+        .expect(200);
+      let usernameId = response.body.hasOwnProperty('usernameId');
+      expect(usernameId).toBe(true);
+      const allPosts = await api.get('/api/post');
+      expect(allPosts.body).toHaveLength(1);
+    });
+
+    it('should throw (401) if user with invalid token', async () => {
+      await api
+        .post('/api/post')
+        .send({
+          title: 'valid post',
+          content: 'testing',
+        })
+        .set({ Authorization: 'Bearer ' + 'invalidToken' })
+        .expect(401);
+    });
+    it('should throw (400) if user with invalid post', async () => {
+      await api
+        .post('/api/post')
+        .send({
+          title: '',
+          content: 'testing',
+        })
+        .set({ Authorization: 'Bearer ' + userToken.token })
         .expect(400);
     });
   });
