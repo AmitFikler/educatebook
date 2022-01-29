d57076f43762a5de228324bc867e9d8695e2e8dd
AmitFikler <amitfikler@gmail.com>
2022-01-29 16:20:19 +0200

    🩹 Fix JWT service

--- backend/src/utils/helpers/jwtService.ts
+++ backend/src/utils/helpers/jwtService.ts
@@ -2,17 +2,14 @@ import jwt, { JwtPayload } from 'jsonwebtoken';
 import config from '../config';
 
 const tokenExtractor = (authorization: string | undefined) => {
-  if (!(authorization && authorization.toLowerCase().startsWith('bearer ')))
-    throw { status: 401, message: 'Token missing' };
-  let decodedToken = null;
   try {
-    decodedToken = jwt.verify(
-      authorization.substring(7),
-      config.secret!
-    ) as JwtPayload;
-    return decodedToken;
+    if (authorization) {
+      const token = authorization.substring(7);
+      const decodedToken = jwt.verify(token, config.secret!) as JwtPayload;
+      return decodedToken;
+    }
   } catch (error) {
-    throw { status: 401, message: 'Invalid token' };
+    throw { status: 401, message: 'Token invalid' };
   }
 };
 
