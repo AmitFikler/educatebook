1f9334d459ed05966de5267c44f7552159533a6c
AmitFikler <amitfikler@gmail.com>
2022-01-29 23:27:20 +0200

    🩹 Add comments in user.ts

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -8,22 +8,24 @@ const getAllUsers = async (
   next: NextFunction
 ) => {
   try {
-    const users = await User.find({});
+    const users = await User.find({}); // get all users
     res.json(users);
   } catch (error) {
     next(error);
   }
 };
+
 const addNewUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const { username, password, role } = req.body;
+    const { username, password, role } = req.body; // get username, password and role from body
     const hashPassword = await bcryptService.hashPassword(password.toString()); // hash password
-    await User.create({
+    const newUser = await User.create({
+      // create new user
       username,
       password: hashPassword,
       role,
     });
-    res.status(201).send('user created!');
+    res.status(201).send(newUser);
   } catch (error) {
     next({ status: 400, message: 'invalid username or role' });
   }
