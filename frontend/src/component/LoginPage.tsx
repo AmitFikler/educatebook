cceccbc5d7372d45caa8be9fdedaaf7bfacac088
AmitFikler <amitfikler@gmail.com>
2022-02-14 20:12:41 +0200

    📁 Create Login Page

---
+++ frontend/src/component/LoginPage.tsx
@@ -0,0 +1,13 @@
+function LoginPage() {
+  return (
+    <>
+      <h1>Welcome To Educatbook!</h1>
+      <h3>Login:</h3>
+      <input type={'text'} placeholder="Email.." />
+      <input type={'password'} placeholder="Password.." />
+      <button>Login</button>
+    </>
+  );
+}
+
+export default LoginPage;
