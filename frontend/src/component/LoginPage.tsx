6b079f55cd30a87044c1bd36839c9368eaf019ee
AmitFikler <amitfikler@gmail.com>
2022-02-14 20:55:16 +0200

    📁 Finish Sign in Component styling

--- frontend/src/component/LoginPage.tsx
+++ frontend/src/component/LoginPage.tsx
@@ -1,12 +1,56 @@
+import {
+  Avatar,
+  Button,
+  Grid,
+  Paper,
+  TextField,
+  Typography,
+} from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import '../styles/loginPage.css';
+import { Link } from 'react-router-dom';
 function LoginPage() {
   return (
-    <>
-      <h1>Welcome To Educatbook!</h1>
-      <h3>Login:</h3>
-      <input type={'text'} placeholder="Email.." />
-      <input type={'password'} placeholder="Password.." />
-      <button>Login</button>
-    </>
+    <Grid spacing={3}>
+      <Paper className="loginPaper" elevation={10}>
+        <Grid align="center">
+          <Avatar style={{ backgroundColor: '#368bff' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <h2>Sign in</h2>
+        </Grid>
+        <TextField
+          label="Username"
+          variant="standard"
+          placeholder="Enter username"
+          fullWidth
+          style={{ margin: '8px 0' }}
+          required
+        />
+        <TextField
+          label="Password"
+          style={{ margin: '8px 0' }}
+          variant="standard"
+          placeholder="Enter password"
+          fullWidth
+          type="password"
+          required
+        />
+        <Button
+          type="submit"
+          color="primary"
+          fullWidth
+          variant="contained"
+          style={{ margin: '8px 0' }}
+        >
+          Sign in
+        </Button>
+        <div>
+          Don't have an account yet?{'  '}
+          <Link to="/signup">Sign Up</Link>
+        </div>
+      </Paper>
+    </Grid>
   );
 }
 
