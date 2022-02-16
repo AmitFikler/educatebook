80504f8d4ee54752b5f4bf938699fd96a094c226
AmitFikler <amitfikler@gmail.com>
2022-02-16 11:18:13 +0200

    📁 Build Feed component

---
+++ frontend/src/component/Feed.tsx
@@ -0,0 +1,9 @@
+function Feed() {
+  return (
+    <>
+      <>feed</>
+    </>
+  );
+}
+
+export default Feed;
--- frontend/src/component/HomePage.tsx
+++ frontend/src/component/HomePage.tsx
@@ -1,9 +1,15 @@
 import SideBarComp from './SideBar';
 import '../styles/homepage.css';
+import TopBar from './TopBar';
+import Feed from './Feed';
 function HomePage() {
   return (
-    <div className="root">
-      <SideBarComp />
+    <div>
+      <TopBar />
+      <div className="homeContainer">
+        <SideBarComp />
+        <Feed />
+      </div>
       <h2>Home Page</h2>
     </div>
   );
