3b965325ce0afdacf05726d4d15277ad58e36801
AmitFikler <amitfikler@gmail.com>
2022-02-17 19:35:15 +0200

    🩹 Update user component

--- frontend/src/component/Comment.tsx
+++ frontend/src/component/Comment.tsx
@@ -13,9 +13,9 @@ function Comment({ comment }: { comment: CommentType }) {
         >
           <div className="postOwner">
             <span style={{ display: 'flex', alignItems: 'center' }}>
-              <Avatar>A</Avatar>
-              <p className="postEmail">amitfikler@gmail.com</p>
-              <h5>-Tutor</h5>
+              <Avatar>{comment.usernameId.username[0].toUpperCase()}</Avatar>
+              <p className="postEmail">{comment.usernameId.username}</p>
+              <h5> | {comment.usernameId.role}</h5>
             </span>
             <p>12/2/2022 16:50</p>
           </div>
