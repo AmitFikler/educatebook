3b882943857c1b8fe13530b7aca5f8673db1ad52
AmitFikler <amitfikler@gmail.com>
2022-02-20 22:08:42 +0200

    🗑️ Remove comments "Provider"

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,9 +6,7 @@ import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <Provider> */}
     <App />
-    {/* </Provider> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
