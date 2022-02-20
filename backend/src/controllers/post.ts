146884fd0437043e5f663e0f826055bd86ffdd26
AmitFikler <amitfikler@gmail.com>
2022-02-20 21:59:35 +0200

    🩹 Making  like  more generic

--- backend/src/controllers/post.ts
+++ backend/src/controllers/post.ts
@@ -22,10 +22,15 @@ const postAPost = async (req: Request, res: Response, next: NextFunction) => {
 };
 
 const likeAPost = async (req: Request, res: Response, next: NextFunction) => {
-  const { postId } = req.body; // get postId from body
+  const { postId, likes, type } = req.body; // get postId, likes and type from body
   const decodedToken = req.decodedToken;
   try {
-    const postToLike = await likeAPostService(postId, decodedToken!.id);
+    const postToLike = await likeAPostService(
+      postId,
+      decodedToken!.id,
+      Number(likes),
+      type
+    );
     res.send(`${postId} has ${postToLike!.likes + 1} likes`);
   } catch (error) {
     next(error);
