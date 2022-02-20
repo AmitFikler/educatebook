93d558d01461f235d20874422d57601506b1281f
AmitFikler <amitfikler@gmail.com>
2022-02-20 22:00:34 +0200

    📁 Make coming soon func component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { useMemo, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
 import { UserType } from '../@types/@types';
 import HomePage from './component/HomePage';
 import LoginPage from './component/LoginPage';
@@ -10,6 +10,13 @@ function App() {
   const [user, setUser] = useState<UserType | null>(null);
 
   const value = useMemo(() => ({ user, setUser }), [user, setUser]);
+
+  const ComingSoon = () => (
+    <div>
+      <h1>Coming Soon</h1>
+      <Link to="/">Back Home</Link>
+    </div>
+  );
   return (
     <div className="App">
       <UserContext.Provider value={value}>
@@ -18,6 +25,8 @@ function App() {
             <Route path="/" element={<HomePage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/signup" element={<SignUpPage />} />
+            <Route path="/chat" element={<ComingSoon />} />
+            <Route path="/profile" element={<ComingSoon />} />
           </Routes>
         </Router>
       </UserContext.Provider>
