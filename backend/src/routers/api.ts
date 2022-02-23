9e1ec98ffb9ad5d893cff4a30cc7dddec365497f
AmitFikler <amitfikler@gmail.com>
2022-02-23 10:31:29 +0200

    🔗 Router /api/message get all data from room

--- backend/src/routers/api.ts
+++ backend/src/routers/api.ts
@@ -4,6 +4,7 @@ import userRouter from './user';
 import loginRouter from './login';
 import postRouter from './post';
 import commentRouter from './comment';
+import messageRouter from './message';
 
 const router = express.Router();
 
@@ -11,5 +12,6 @@ router.use('/user', userRouter);
 router.use('/login', loginRouter);
 router.use('/post', postRouter);
 router.use('/comment', commentRouter);
+router.use('/message', messageRouter);
 
 export default router;
