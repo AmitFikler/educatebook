03059fc9ae88e8e9e7964a83a3aa1443eabd494f
AmitFikler <amitfikler@gmail.com>
2022-02-28 22:43:45 +0200

    ✨ Added Loader

--- frontend/src/component/SignAndLog/SignUpPage.tsx
+++ frontend/src/component/SignAndLog/SignUpPage.tsx
@@ -15,6 +15,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import '../../styles/signUpPage.css';
 import { useState } from 'react';
+import { ReactComponent as Spinner } from '../../images/spinner.svg';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
@@ -26,6 +27,7 @@ function SignUpPage() {
   const [password, setPassword] = useState<string | undefined>();
   const [role, setRole] = useState<'student' | 'tutor' | undefined>();
   const [picture, setPicture] = useState<string | ArrayBuffer | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   /********* NAVIGATE *********/
   const navigate = useNavigate();
@@ -33,6 +35,7 @@ function SignUpPage() {
   /********* Sign-Up *********/
   const handleSignUp = async () => {
     try {
+      setIsLoading(true);
       await axios.post(`${process.env.REACT_APP_SERVER_URI}/api/user`, {
         fullName,
         email,
@@ -40,6 +43,7 @@ function SignUpPage() {
         role,
         picture,
       });
+      setIsLoading(false);
       navigate('/login');
       toast('Sign up successfully', {
         type: 'success',
@@ -67,108 +71,115 @@ function SignUpPage() {
   };
 
   return (
-    <Grid>
-      <Paper className="signUpPaper" elevation={10}>
-        <Grid className="signUpHeader">
-          <Avatar style={{ backgroundColor: '#368bff' }}>
-            <AddCircleIcon />
-          </Avatar>
-          <h2>Sign up</h2>
-        </Grid>
-        <Typography variant="caption">
-          Please fill this form to create an account
-        </Typography>
-        <TextField
-          label="Full Name"
-          variant="standard"
-          type="text"
-          placeholder="Enter your full name"
-          onChange={(e) => setFullName(e.target.value)}
-          fullWidth
-          style={{ margin: '8px 0' }}
-          required
-        />
-        <TextField
-          label="Email"
-          variant="standard"
-          type="email"
-          placeholder="Enter email"
-          onChange={(e) => setEmail(e.target.value)}
-          fullWidth
-          style={{ margin: '8px 0' }}
-          required
-        />
-        <FormControl
-          component="fieldset"
-          style={{ marginTop: '5px' }}
-          onChange={(e: any) => setRole(e.target.value)} //TODO -type any
-        >
-          <FormLabel component="legend" required>
-            Role
-          </FormLabel>
-          <RadioGroup
-            aria-label="role"
-            name="role"
-            style={{ display: 'initial' }}
+    <>
+      <Grid>
+        <Paper className="signUpPaper" elevation={10}>
+          <Grid className="signUpHeader">
+            <Avatar style={{ backgroundColor: '#368bff' }}>
+              <AddCircleIcon />
+            </Avatar>
+            <h2>Sign up</h2>
+          </Grid>
+          <Typography variant="caption">
+            Please fill this form to create an account
+          </Typography>
+          <TextField
+            label="Full Name"
+            variant="standard"
+            type="text"
+            placeholder="Enter your full name"
+            onChange={(e) => setFullName(e.target.value)}
+            fullWidth
+            style={{ margin: '8px 0' }}
+            required
+          />
+          <TextField
+            label="Email"
+            variant="standard"
+            type="email"
+            placeholder="Enter email"
+            onChange={(e) => setEmail(e.target.value)}
+            fullWidth
+            style={{ margin: '8px 0' }}
+            required
+          />
+          <FormControl
+            component="fieldset"
+            style={{ marginTop: '5px' }}
+            onChange={(e: any) => setRole(e.target.value)} //TODO -type any
           >
-            <FormControlLabel
-              value="student"
-              control={<Radio />}
-              label="Student"
-            />
-            <FormControlLabel value="tutor" control={<Radio />} label="Tutor" />
-          </RadioGroup>
-        </FormControl>
-        <TextField
-          label="Password"
-          style={{ margin: '8px 0' }}
-          variant="standard"
-          placeholder="Enter password"
-          fullWidth
-          onChange={(e) => setPassword(e.target.value)}
-          type="password"
-          required
-        />
-        <div className="upload-img">
-          <Button variant="contained" component="label">
-            <AddPhotoAlternateIcon /> Upload Profile Picture
-            <input
-              type="file"
-              hidden
-              name="img"
-              accept="image/*"
-              onChange={imageHandler}
-            />
+            <FormLabel component="legend" required>
+              Role
+            </FormLabel>
+            <RadioGroup
+              aria-label="role"
+              name="role"
+              style={{ display: 'initial' }}
+            >
+              <FormControlLabel
+                value="student"
+                control={<Radio />}
+                label="Student"
+              />
+              <FormControlLabel
+                value="tutor"
+                control={<Radio />}
+                label="Tutor"
+              />
+            </RadioGroup>
+          </FormControl>
+          <TextField
+            label="Password"
+            style={{ margin: '8px 0' }}
+            variant="standard"
+            placeholder="Enter password"
+            fullWidth
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            required
+          />
+          <div className="upload-img">
+            <Button variant="contained" component="label">
+              <AddPhotoAlternateIcon /> Upload Profile Picture
+              <input
+                type="file"
+                hidden
+                name="img"
+                accept="image/*"
+                onChange={imageHandler}
+              />
+            </Button>
+            {picture && (
+              <img
+                src={`${picture}`}
+                alt="profile"
+                style={{
+                  width: '100px',
+                  height: '100px',
+                  borderRadius: '50%',
+                  margin: '8px 0',
+                }}
+              />
+            )}
+          </div>
+          <Button
+            type="submit"
+            color="primary"
+            fullWidth
+            variant="contained"
+            style={{ margin: '8px 0' }}
+            onClick={() => handleSignUp()}
+          >
+            Sign Up
           </Button>
-          {picture && (
-            <img
-              src={`${picture}`}
-              alt="profile"
-              style={{
-                width: '100px',
-                height: '100px',
-                borderRadius: '50%',
-                margin: '8px 0',
-              }}
-            />
-          )}
-        </div>
-        <Button
-          type="submit"
-          color="primary"
-          fullWidth
-          variant="contained"
-          style={{ margin: '8px 0' }}
-          onClick={() => handleSignUp()}
-        >
-          Sign Up
-        </Button>
-        <div>
-          Already have an account?{'  '}
-          <Link to="/login">Sign In</Link>
-        </div>
-      </Paper>
-    </Grid>
+          <div>
+            Already have an account?{'  '}
+            <Link to="/login">Sign In</Link>
+          </div>
+          {isLoading && <Spinner />}
+        </Paper>
+      </Grid>
+    </>
   );
 }
 
